bbcd0a1c5dcb30be68e26394ebbe3b216c01c2bd
amtoaer <amtoaer@gmail.com>
2021-02-02 18:12:45 +0800

    feat: add sdk configuration and use it in main.go

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"github.com/amtoaer/fabric-backend/sdk"
+)
+
+const (
+	configFile  = "config.yaml"
+	initialized = false
+	// ChainCode 链码名
+	ChainCode = "simplecc"
+)
+
+func main() {
+	initInfo := &sdk.InitInfo{
+		ChannelID:      "kevinkongyixueyuan",
+		ChannelConfig:  "./fixtures/artifacts/channel.tx",
+		OrgAdmin:       "Admin",
+		OrgName:        "Org1",
+		OrdererOrgName: "orderer.kevin.kongyixueyuan.com",
+	}
+	mySDK, err := sdk.SetupSDK(configFile, initialized)
+	if err != nil {
+		panic(err)
+	}
+	defer mySDK.Close()
+
+	err = sdk.CreateChannel(mySDK, initInfo)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
