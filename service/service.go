ab2706b46429352baf5d631c3d95f027d995e47e
amtoaer <amtoaer@gmail.com>
2021-02-07 21:19:58 +0800

    feat(service): implements service interface

--- service/service.go
+++ service/service.go
@@ -1,25 +1,75 @@
 package service
 
-import "github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+import (
+	"encoding/json"
+	"fmt"
 
-// 测试是否实现接口
-var _ Service = &internal{}
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+)
 
-// 实现服务的内部类
-type internal struct {
-	chaincodeID string
-	client      *channel.Client
-}
-
-// Service 用于与底层链码交互，执行功能的接口，由内部internal类实现该接口
-type Service interface {
-	// TODO: 需实现的方法列表
-}
-
-// NewService 返回Service方便调用
-func NewService(chaincodeID string, client *channel.Client) Service {
-	return &internal{
-		chaincodeID: chaincodeID,
-		client:      client,
+func (i *internal) AddRecord(record Record) (string, error) {
+	eventID := "eventAddRecord"
+	reg, notifier := registerEvent(i.client, i.chaincodeID, eventID)
+	defer i.client.UnregisterChaincodeEvent(reg)
+	bytes, err := json.Marshal(record)
+	if err != nil {
+		return "", fmt.Errorf("指定对象序列化失败。")
 	}
+	req := channel.Request{ChaincodeID: i.chaincodeID, Fcn: "addRecord", Args: [][]byte{bytes, []byte(eventID)}}
+	response, err := i.client.Execute(req)
+	if err != nil {
+		return "", err
+	}
+	err = eventResult(notifier, eventID)
+	if err != nil {
+		return "", err
+	}
+	return string(response.TransactionID), nil
+}
+
+func (i *internal) UpdateRecord(record Record) (string, error) {
+	eventID := "eventUpdateRecord"
+	reg, notifier := registerEvent(i.client, i.chaincodeID, eventID)
+	defer i.client.UnregisterChaincodeEvent(reg)
+	bytes, err := json.Marshal(record)
+	if err != nil {
+		return "", fmt.Errorf("指定对象序列化失败。")
+	}
+	req := channel.Request{ChaincodeID: i.chaincodeID, Fcn: "updateRecord", Args: [][]byte{bytes, []byte(eventID)}}
+	response, err := i.client.Execute(req)
+	if err != nil {
+		return "", err
+	}
+	err = eventResult(notifier, eventID)
+	if err != nil {
+		return "", err
+	}
+	return string(response.TransactionID), nil
+}
+
+func (i *internal) QueryRecordByPatientID(patientID string) (string, error) {
+	req := channel.Request{ChaincodeID: i.chaincodeID, Fcn: "queryRecordByPatientID", Args: [][]byte{[]byte(patientID)}}
+	resp, err := i.client.Query(req)
+	if err != nil {
+		return "", err
+	}
+	return string(resp.Payload), nil
+}
+
+func (i *internal) QueryRecordByDoctorID(doctorID string) (string, error) {
+	req := channel.Request{ChaincodeID: i.chaincodeID, Fcn: "queryRecordByDoctorID", Args: [][]byte{[]byte(doctorID)}}
+	resp, err := i.client.Query(req)
+	if err != nil {
+		return "", err
+	}
+	return string(resp.Payload), nil
+}
+
+func (i *internal) QueryRecordByKey(patientID, doctorID string) (string, error) {
+	req := channel.Request{ChaincodeID: i.chaincodeID, Fcn: "queryRecordByKey", Args: [][]byte{[]byte(patientID + doctorID)}}
+	resp, err := i.client.Query(req)
+	if err != nil {
+		return "", err
+	}
+	return string(resp.Payload), nil
 }
