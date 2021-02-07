ee7f07715bc04d7311cc421635cca3ec4f32df77
amtoaer <amtoaer@gmail.com>
2021-02-07 18:36:07 +0800

    feat(service): support event

---
+++ service/event.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
+)
+
+func regitserEvent(client *channel.Client, chaincodeID, eventID string) (fab.Registration, <-chan *fab.CCEvent) {
+	reg, notifier, err := client.RegisterChaincodeEvent(chaincodeID, eventID)
+	if err != nil {
+		fmt.Printf("注册链码事件失败: %v\n", err)
+	}
+	return reg, notifier
+}
+
+func eventResult(notifier <-chan *fab.CCEvent, eventID string) error {
+	select {
+	case ccEvent := <-notifier:
+		fmt.Printf("接收到链码事件: %v\n", ccEvent)
+	case <-time.After(time.Second * 20):
+		return fmt.Errorf("不能根据指定的事件ID接收到相应的链码事件(%s)", eventID)
+	}
+	return nil
+}
